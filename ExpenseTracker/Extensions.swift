a5d6dbe2fa2c4a83f864b16239796731b5b03593
tromao <tiagomromao@gmail.com>
2022-06-23 22:13:02 +0100

    Starting building the main screen

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Tiago Miguel de Jesus Romao on 23/06/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}

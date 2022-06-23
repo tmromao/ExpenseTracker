4217a4f0ea54c8b5cb6df3c2a70e0f7d1c95c225
tromao <tiagomromao@gmail.com>
2022-06-23 21:58:11 +0100

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Tiago Miguel de Jesus Romao on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
